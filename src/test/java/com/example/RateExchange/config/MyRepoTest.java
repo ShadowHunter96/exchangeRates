5be9ec2e762ad2cf595eee4b15371cc66db3309a
marekvu <marekvu1@email.cz>
2025-11-05 00:10:01 +0100

    added yml file

--- src/test/java/com/example/RateExchange/config/MyRepoTest.java
+++ src/test/java/com/example/RateExchange/config/MyRepoTest.java
@@ -1,25 +1,24 @@
 package com.example.RateExchange.config;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-/**
- * Created by User: Vu
- * Date: 04.11.2025
- * Time: 23:57
- */
 @Testcontainers
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MyRepoTest {
 
     @Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
     @Test
-    void works() {}
+    void works() {
+        // … test repository / JPA logiku
+    }
 }
